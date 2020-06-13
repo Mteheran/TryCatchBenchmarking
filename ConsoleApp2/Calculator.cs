ce313dbbb7ce41ae23cbbb2f00eca3bfaf7cc2ad
Miguel teheran <miguelteheran@gmail.com>
2020-06-13 18:03:39 -0500

    Base code

---
+++ ConsoleApp2/Calculator.cs
@@ -0,0 +1,47 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using System;
+
+namespace TryCatchBenchmarking
+{
+
+	[SimpleJob(RuntimeMoniker.NetCoreApp30)]
+	[RPlotExporter]
+	public class Calculator
+    {
+
+		[Benchmark]
+		[BenchmarkCategory("Run")]
+		public double GetDivision() => Division();
+
+		[Benchmark]
+		[BenchmarkCategory("Run")]
+		public double GetDivisionDivisionException() => DivisionException();
+
+		[Params(0, 1, 2, 3)]
+		public int number1 { get; set; }
+
+		[Params(0, 1, 2, 3)]
+		public int number2 { get; set; }
+
+		public double Division()
+		{
+			if (number2 == 0) return double.NaN;
+			
+			return number1 / number2;
+		}
+
+		public double DivisionException()
+        {
+			try
+			{
+				return number1 / number2;
+			}
+			catch (Exception)
+			{
+
+				return double.NaN;
+			}
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace TryCatchBenchmarking
+{
+    [SimpleJob(RuntimeMoniker.NetCoreApp30)]
+    [RPlotExporter]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Calculator>();
+
+            Console.ReadLine();
+        }
+
+    }
+}
